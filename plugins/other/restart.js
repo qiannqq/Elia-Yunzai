b90a15a1a73fb2cd6ab0ded3fc2d2bdd2e57a5db
千奈千祁 <2632139786@qq.com>
2025-06-30 18:57:50 +0800

    feat: 重启逻辑优化

--- plugins/other/restart.js
+++ plugins/other/restart.js
@@ -1,47 +1,44 @@
-import plugin from "../../lib/plugins/plugin.js"
-import { createRequire } from "module"
-import fetch from "node-fetch"
-import net from "net"
-import fs from "fs"
-import YAML from "yaml"
+import plugin from '../../lib/plugins/plugin.js'
+import { createRequire } from 'module'
+import fetch from 'node-fetch'
+import net from 'net'
+import fs from 'fs'
+import YAML from 'yaml'
+import common from '../../lib/common/common.js'
 
 const require = createRequire(import.meta.url)
-const { exec } = require("child_process")
+const { exec } = require('child_process')
 
-const isPortTaken = async port => {
-  return new Promise(resolve => {
-    const tester = net
-      .createServer()
-      .once("error", () => resolve(true))
-      .once("listening", () => tester.once("close", () => resolve(false)).close())
-      .listen(port)
-  })
-}
+const isPortTaken = async (port) => {
+  return new Promise((resolve) => {
+    const tester = net.createServer()
+      .once('error', () => resolve(true))
+      .once('listening', () => tester.once('close', () => resolve(false)).close())
+      .listen(port);
+  });
+};
 
 export class Restart extends plugin {
-  constructor(e = "") {
+  constructor(e = '') {
     super({
-      name: "重启",
-      dsc: "#重启",
-      event: "message",
+      name: '重启',
+      dsc: '#重启',
+      event: 'message',
       priority: 10,
-      rule: [
-        {
-          reg: "^#重启$",
-          fnc: "restart",
-          permission: "master",
-        },
-        {
-          reg: "^#(停机|关机)$",
-          fnc: "stop",
-          permission: "master",
-        },
-      ],
+      rule: [{
+        reg: '^#重启$',
+        fnc: 'restart',
+        permission: 'master'
+      }, {
+        reg: '^#(停机|关机)$',
+        fnc: 'stop',
+        permission: 'master'
+      }]
     })
 
     if (e) this.e = e
 
-    this.key = "Yz:restart"
+    this.key = 'Yz:restart'
   }
 
   async init() {
@@ -54,10 +51,20 @@ export class Restart extends plugin {
 
       let msg = `重启成功：耗时${time.toFixed(2)}秒`
       try {
-        if (restart.isGroup) {
-          Bot[uin].pickGroup(restart.id).sendMsg(msg)
-        } else {
-          Bot[uin].pickUser(restart.id).sendMsg(msg)
+        /** 等待Bot初始化，超出60秒则放弃回复 */
+        for (let i = 0; i < 60; i++) {
+          if (restart.isGroup) {
+            if (Bot[uin]?.pickGroup(restart.id)) {
+              Bot[uin].pickGroup(restart.id).sendMsg(msg)
+              break
+            }
+          } else {
+            if (Bot[uin]?.pickUser(restart.id)) {
+              Bot[uin].pickUser(restart.id).sendMsg(msg)
+              break
+            }
+          }
+          await common.sleep(1000)
         }
       } catch (error) {
         /** 不发了，发不出去... */
@@ -72,15 +79,15 @@ export class Restart extends plugin {
     try {
       restart_port = YAML.parse(fs.readFileSync(`./config/config/bot.yaml`, `utf-8`))
       restart_port = restart_port.restart_port || 27881
-    } catch {}
-    await this.e.reply("开始执行重启，请稍等...")
+    } catch { }
+    await this.e.reply('开始执行重启，请稍等...')
     logger.mark(`${this.e.logFnc} 开始执行重启，请稍等...`)
 
     let data = JSON.stringify({
       uin: this.e?.self_id || this.e.bot.uin,
       isGroup: !!this.e.isGroup,
       id: this.e.isGroup ? this.e.group_id : this.e.user_id,
-      time: new Date().getTime(),
+      time: new Date().getTime()
     })
 
     let npm = await this.checkPnpm()
@@ -101,7 +108,7 @@ export class Restart extends plugin {
     } else {
       try {
         let cm = `${npm} start`
-        if (process.argv[1].includes("pm2")) {
+        if (process.argv[1].includes('pm2')) {
           cm = `${npm} run restart`
         }
 
@@ -111,7 +118,7 @@ export class Restart extends plugin {
             this.e.reply(`操作失败！\n${error.stack}`)
             logger.error(`重启失败\n${error.stack}`)
           } else if (stdout) {
-            logger.mark("重启成功，运行已由前台转为后台")
+            logger.mark('重启成功，运行已由前台转为后台')
             logger.mark(`查看日志请用命令：${npm} run log`)
             logger.mark(`停止后台运行命令：${npm} stop`)
             process.exit()
@@ -128,9 +135,9 @@ export class Restart extends plugin {
   }
 
   async checkPnpm() {
-    let npm = "npm"
-    let ret = await this.execSync("pnpm -v")
-    if (ret.stdout) npm = "pnpm"
+    let npm = 'npm'
+    let ret = await this.execSync('pnpm -v')
+    if (ret.stdout) npm = 'pnpm'
     return npm
   }
 
@@ -147,10 +154,10 @@ export class Restart extends plugin {
     try {
       restart_port = YAML.parse(fs.readFileSync(`./config/config/bot.yaml`, `utf-8`))
       restart_port = restart_port.restart_port || 27881
-    } catch {}
+    } catch { }
     if (await isPortTaken(restart_port || 27881)) {
       try {
-        logger.mark("关机成功，已停止运行")
+        logger.mark('关机成功，已停止运行')
         await this.e.reply(`关机成功，已停止运行`)
         await fetch(`http://localhost:${restart_port || 27881}/exit`)
         return
@@ -160,14 +167,14 @@ export class Restart extends plugin {
       }
     }
 
-    if (!process.argv[1].includes("pm2")) {
-      logger.mark("关机成功，已停止运行")
-      await this.e.reply("关机成功，已停止运行")
+    if (!process.argv[1].includes('pm2')) {
+      logger.mark('关机成功，已停止运行')
+      await this.e.reply('关机成功，已停止运行')
       process.exit()
     }
 
-    logger.mark("关机成功，已停止运行")
-    await this.e.reply("关机成功，已停止运行")
+    logger.mark('关机成功，已停止运行')
+    await this.e.reply('关机成功，已停止运行')
 
     let npm = await this.checkPnpm()
     exec(`${npm} stop`, { windowsHide: true }, (error, stdout, stderr) => {
