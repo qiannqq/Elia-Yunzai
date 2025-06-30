1ab948aa9760c468accef8ba96086ec193e973d1
千奈千祁 <2632139786@qq.com>
2025-06-30 18:45:25 +0800

    chore: 改名字改名字~

--- plugins/other/update.js
+++ plugins/other/update.js
@@ -264,7 +264,7 @@ export class update extends plugin {
     return common.makeForwardMsg(
       this.e,
       [log, end],
-      `${plugin || "Miao-Yunzai"} 更新日志，共${line}条`,
+      `${plugin || "Elia-Yunzai"} 更新日志，共${line}条`,
     )
   }
 
