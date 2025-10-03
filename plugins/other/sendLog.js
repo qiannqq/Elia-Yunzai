25c1d16350c90227b58897b980322b21ff31df09
千奈千祁 <2632139786@qq.com>
2025-10-04 00:53:36 +0800

    feat: 发送日志可选使用“#控制台日志”。详见README.md

--- plugins/other/sendLog.js
+++ plugins/other/sendLog.js
@@ -1,5 +1,6 @@
 import plugin from "../../lib/plugins/plugin.js"
 import common from "../../lib/common/common.js"
+import puppeteer from '../../lib/puppeteer/puppeteer.js'
 import fs from "node:fs"
 import lodash from "lodash"
 import moment from "moment"
@@ -12,7 +13,7 @@ export class sendLog extends plugin {
       event: "message",
       rule: [
         {
-          reg: "^#(运行|错误)*日志[0-9]*(.*)",
+          reg: "^#(控制台)?(运行|错误)*日志[0-9]*(.*)",
           fnc: "sendLog",
           permission: "master",
         },
@@ -24,6 +25,33 @@ export class sendLog extends plugin {
 
     this.logFile = `logs/command.${moment().format("YYYY-MM-DD")}.log`
     this.errFile = "logs/error.log"
+    this.consoleHtml = 'resources/cmd.html'
+
+    this.colorMap = {
+      '30': 'color-black',
+      '31': 'color-red',
+      '32': 'color-green',
+      '33': 'color-yellow',
+      '34': 'color-blue',
+      '35': 'color-magenta',
+      '36': 'color-cyan',
+      '37': 'color-white',
+      '90': 'color-gray',
+      '91': 'color-red',
+      '92': 'color-green',
+      '93': 'color-yellow',
+      '94': 'color-blue',
+      '95': 'color-magenta',
+      '96': 'color-cyan',
+      '97': 'color-white'
+    };
+    this.logLevelList = {
+      'MARK': '90',
+      'ERRO': '91',
+      'WARN': '93',
+      'INFO': '32',
+      'DEBU': '94',
+    }
   }
 
   async sendLog() {
@@ -31,7 +59,7 @@ export class sendLog extends plugin {
     if (lineNum) {
       this.lineNum = lineNum[0]
     } else {
-      this.keyWord = this.e.msg.replace(/#|运行|错误|日志|\d/g, "")
+      this.keyWord = this.e.msg.replace(/#(控制台)?|运行|错误|日志|\d/g, "")
     }
 
     let logFile = this.logFile
@@ -40,6 +68,8 @@ export class sendLog extends plugin {
       logFile = this.errFile
       type = "错误"
     }
+
+    if (this.e.msg.includes("控制台")) return this.sendConsoleLog()
 
     if (this.keyWord) type = this.keyWord
 
@@ -50,6 +80,58 @@ export class sendLog extends plugin {
     return this.reply(
       await common.makeForwardMsg(this.e, [log.join("\n")], `最近${log.length}条${type}日志`),
     )
+  }
+  async sendConsoleLog() {
+    let type = this.e.msg.includes("错误") ? "错误" : "运行"
+    let logFile = this.logFile
+    if(type == '错误') logFile = this.errFile
+
+    let logContent
+    try {
+      logContent = fs.readFileSync(logFile, 'utf-8')
+    } catch (error) {
+      return this.reply(`读取${type}日志失败：${error.message}`)
+    }
+
+    if(this.lineNum > this.maxNum) this.lineNum = this.maxNum
+
+    logContent = logContent.split('\n')
+    logContent = logContent.slice(-this.lineNum)
+
+    logContent = logContent.map((item) => {
+      let logLevel = item.match(/\[(.*?)\]\[(.*?)\]/);
+      if (logLevel && this.logLevelList[logLevel[2]]) {
+        let reg = new RegExp(`\\[(.*?)\\]\\[${logLevel[2]}\\]`);
+        item = item.replace(reg, `\x1b[${this.logLevelList[logLevel[2]]}m[${logLevel[1]}][${logLevel[2]}]\x1b[39m`);
+      }
+      return this.parseAnsiColors(item);
+    })
+
+    try {
+      let img = await puppeteer.screenshot('renderC', {
+        imgType: "jpeg",
+        quality: 100,
+        tplFile: this.consoleHtml,
+        data: logContent,
+      })
+      await this.e.reply(img)
+    } catch (renderErr) {
+      logger.error(`[渲染控制台] 渲染失败: ${renderErr.message}`)
+      return e.reply(`渲染失败: ${renderErr.message}`), true
+    }
+  }
+
+  parseAnsiColors(text) {
+    if (!text) return '';
+    for (let a in this.colorMap) {
+      let reg = new RegExp(`\x1b\\[${a}m`, 'g')
+      text = text.replace(reg, `<span class="${this.colorMap[a]}">`)
+    }
+    if (/\x1b\[38;\d;\d+;\d+;\d+m/.test(text)) {
+      let rgb = text.match(/\x1b\[38;\d;(\d+);(\d+);(\d+)m/)
+      text = text.replace(/\x1b\[38;\d;\d+;\d+;\d+m/, `<span style="color: rgb(${rgb[1]}, ${rgb[2]}, ${rgb[3]})">`)
+    }
+    return text.replace(/\x1b\[39m/g, '</span>')
   }
 
   getLog(logFile) {
